bc38991a90e9d85e0836b8598308f968fb9465af
daezel <daezelgoyal01@gmail.com>
2024-05-27 17:16:40 +1000

    commit final

---
+++ src/test/java/com/example/demo/CalculatorrTest.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class CalculatorrTest {
+    @Test
+    public void testAddition() {
+        Calculatorr calculator = new Calculatorr();
+        int result = calculator.add(8, 4);
+        assertTrue(result == 12, "The addition of 8 and 4 is 12");
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculatorr calculator = new Calculatorr();
+        int result = calculator.subtract(8, 4);
+        assertTrue(result == 4, "The subtract of 8 and 4 should be 4");
+    }
+
+    @Test
+    public void testMultiplication() {
+        Calculatorr calculator = new Calculatorr();
+        int result = calculator.multiply(8, 4);
+        assertTrue(result == 32, "The multiply of 8 and 4 should be 32");
+    }
+
+    @Test
+    public void testDivision() {
+        Calculatorr calculator = new Calculatorr();
+        int result = calculator.divide(8, 4);
+        assertTrue(result == 2, "The division of 8 and 4 should be 2");
+    }
+}
