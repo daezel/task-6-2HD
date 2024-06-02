ac146f1baee67f59328a1d7734fac918858bfdae
daezel <daezelgoyal01@gmail.com>
2024-06-02 13:07:40 +1000

    modified test

--- src/test/java/com/example/demo/CalculatorrTest.java
+++ src/test/java/com/example/demo/CalculatorrTest.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;;
 
 @SpringBootTest
 class CalculatorrTest {
@@ -10,27 +10,27 @@ class CalculatorrTest {
     public void testAddition() {
         Calculatorr calculator = new Calculatorr();
         int result = calculator.add(8, 4);
-        assertTrue(result == 12, "The addition of 8 and 4 is 12");
+        assertEquals(result == 12, "The addition of 8 and 4 is 12");
     }
 
     @Test
     public void testSubtract() {
         Calculatorr calculator = new Calculatorr();
         int result = calculator.subtract(8, 4);
-        assertTrue(result == 4, "The subtract of 8 and 4 should be 4");
+        assertEquals(result == 4, "The subtract of 8 and 4 should be 4");
     }
 
     @Test
     public void testMultiplication() {
         Calculatorr calculator = new Calculatorr();
         int result = calculator.multiply(8, 4);
-        assertTrue(result == 32, "The multiply of 8 and 4 should be 32");
+        assertEquals(result == 32, "The multiply of 8 and 4 should be 32");
     }
 
     @Test
     public void testDivision() {
         Calculatorr calculator = new Calculatorr();
         int result = calculator.divide(8, 4);
-        assertTrue(result == 2, "The division of 8 and 4 should be 2");
+        assertEquals(result == 2, "The division of 8 and 4 should be 2");
     }
 }
